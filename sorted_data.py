1a38ad75d783e17073fe98c0794ed8a80c6bb455
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 17:21:28 -0700

    Working ratings program

--- sorted_data.py
+++ sorted_data.py
@@ -2,5 +2,11 @@ ratings = open("scores.txt")
 restaurants = {}
 
 for line in ratings:
-    name, rating = line.split(":")
-    
+    name, rating = line.rstrip().split(":")
+    restaurants[name] = rating
+
+rest_list = restaurants.keys()
+rest_list.sort()
+
+for item in rest_list:
+    print "Restaurant %s is rated at %s" % (item, restaurants[item])
